e13ff763e6eefba967d6ad32c26410fb0440f414
samydoesit <sxburk@gmail.com>
2022-09-17 02:07:23 +0200

    chore: edit tsconfig setup

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -1,4 +1,4 @@
 // import 'virtual:windi.css'
-import CustomButton from './components/CustomButton/CustomButton.vue'
+import CustomButton from './components/button/Button.vue'
 import CustomInput from './components/CustomInput/CustomInput.vue'
 export { CustomButton, CustomInput }
