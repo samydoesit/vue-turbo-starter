5b4a29355463f5a4d11d46fa78477d5e3c38ca60
samydoesit <sxburk@gmail.com>
2022-10-10 18:38:11 +0200

    fix: fix vitest

--- packages/vitest/vitest.config.ts
+++ packages/vitest/vitest.config.ts
@@ -1,8 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
-import config from './vite.config'
+// import config from './vite.config'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  ...config,
+  // ...config,
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@ui': resolve(__dirname, '../ui/src')
+      // '@ui': fileURLToPath(new URL('../ui/src', import.meta.url))
+    }
+  },
   test: {
     include: [
       '../ui/src/components/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
