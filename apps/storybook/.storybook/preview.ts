8f04fdc5a9ac313189361137b4c6f5187ae2c52c
samydoesit <sxburk@gmail.com>
2022-10-14 01:15:42 +0200

    chore: update storybook to v7

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,8 +1,9 @@
-// import { app } from '@storybook/vue3'
+import { setup } from '@storybook/vue3'
 import * as jest from '@storybook/jest'
+import { action } from '@storybook/addon-actions'
 
 import '@unocss/reset/tailwind.css'
-import 'virtual:uno.css'
+import 'uno.css'
 
 // Fix: fn() is not defined, see: https://github.com/storybookjs/storybook/issues/15391
 window.jest = jest as any
@@ -12,6 +13,24 @@ window.jest = jest as any
 // useUser()
 // import { useUser } from '../store/user'
 // const pinia = createPinia()
+
+setup((app) => {
+  // Provide NuxtLink Stub
+  app.component('NuxtLink', {
+    props: {
+      to: {
+        type: String,
+        default: '',
+      },
+    },
+    methods: {
+      log () {
+        action('link target')(this.to)
+      },
+    },
+    template: '<div @click="log()"><slot>NuxtLink</slot></div>',
+  })
+})
 
 export const parameters = {
   layout: 'centered',
