c64823b07f4e7e69a6cc47b13f50a44b18d379b5
samydoesit <sxburk@gmail.com>
2022-10-26 17:01:23 +0200

    chore: remove vite app

--- apps/vite/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import '@unocss/reset/tailwind.css'
-import 'virtual:uno.css'
-
-createApp(App).use(createPinia()).mount('#app')
