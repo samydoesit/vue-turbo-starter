88b3ff2f6fcdf5de2ed2c5ad33911df38bbc4eb3
samydoesit <sxburk@gmail.com>
2023-03-25 12:34:56 +0100

    chore(deps): update storybook v7 rc8

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -6,37 +6,28 @@ import remarkGfm from 'remark-gfm'
 // import { alias } from '../../../alias'
 
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-    '../../../packages/ui/src/components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-  ],
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)', '../../../packages/ui/src/components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
   },
-  addons: [
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docs: false,
-      },
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      docs: false,
     },
-    '@storybook/addon-interactions',
-    '@storybook/addon-links',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            // MDX2 turned off Github flavoured markdown
-            // https://storybook.js.org/docs/7.0/react/writing-docs/mdx#lack-of-github-flavored-markdown-gfm
-            remarkPlugins: [remarkGfm],
-          },
+  }, '@storybook/addon-interactions', '@storybook/addon-links', {
+    name: '@storybook/addon-docs',
+    options: {
+      mdxPluginOptions: {
+        mdxCompileOptions: {
+          // MDX2 turned off Github flavoured markdown
+          // https://storybook.js.org/docs/7.0/react/writing-docs/mdx#lack-of-github-flavored-markdown-gfm
+          remarkPlugins: [remarkGfm],
         },
       },
     },
-  ],
-  // https://github.com/storybookjs/storybook/issues/20583
+  }], // https://github.com/storybookjs/storybook/issues/20583
   // viteFinal (config) {
   //   config.resolve.alias = {
   //     ...config.resolve.alias,
@@ -51,6 +42,8 @@ const config: StorybookConfig = {
   //   // return the customized config
   //   return config
   // },
+  docs: {
+    autodocs: true,
+  },
 }
-
 export default config
